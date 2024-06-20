68bdb83a3fbbd3241a81998152deb5f17ed52c73
Alex <alex.holder@bigfootds.com>
2024-06-20 20:54:43 +1000

    feat: defined blog model

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
 # oct23-express-blog-api
+
+## Models to make
+
+### Blog
+
+- Title
+- Content
+- User (posted by)
+- Created date
+- Like 
+- Image upload 
+- Category/Tags/keywords 
+- Audit history
+	- user 
+	- timestamp 
+
+
+### Users 
+
+- username
+- blog post view history 
+
+### Comments 
+Join table in SQL, but subdocument in Mongoose that lives in Blog posts 
+- user id 
+- comment content 
+- like
+
+
+### Action Log 
+- user id 
+- route visited
+- timestamp 
+- result
---
+++ src/models/BlogModel.js
@@ -0,0 +1,60 @@
+/**
+- Title
+- Content
+- User (posted by)
+- Like 
+- Image upload 
+- Category/Tags/keywords 
+- Audit history
+	- user 
+	- timestamp 
+*/
+
+const mongoose = require("mongoose");
+
+const blogSchema = mongoose.Schema({
+	title: {
+		type: String,
+		required: true
+	},
+	content: {
+		type: String, 
+		required: true
+	},
+	author: {
+		type: String, // come back later and replace this with a Mongoose object ID 
+		required: true
+	},
+	likes: {
+		type: [String], // come back later and replace this with a Mongoose object ID ,
+		required: false
+	},
+	headerImage: {
+		type: String, // URL to the file/image storage provider 
+		required: true
+	},
+	tags: { // keywords defined by the blog post author
+		type: [String], // ["life", "travel", "photography"]
+		required: true
+	},
+	categories: { // post category defined by website admin/developer 
+		type: [String], // ["life", "travel", "photography"]
+		enum: ["life", "travel", "photography", "coding"],
+		required: true
+	},
+	editHistory: {
+		type: [{user: String, timestamp: Date}],
+		required: false
+	}
+},
+{
+	timestamps: true
+});
+
+
+const BlogModel = mongoose.model("Blog", blogSchema);
+
+module.exports = {
+	BlogModel
+}
+
