1faac40c6c270bfb8143140a4884ebba69acd654
Alex <alex.holder@bigfootds.com>
2024-06-22 09:39:33 +1000

    feat: refactor blog to refer to user ids

--- src/models/BlogModel.js
+++ src/models/BlogModel.js
@@ -22,11 +22,11 @@ const blogSchema = mongoose.Schema({
 		required: true
 	},
 	author: {
-		type: String, // come back later and replace this with a Mongoose object ID 
+		type: {type: mongoose.Schema.Types.ObjectId, ref: "User"},
 		required: true
 	},
 	likes: {
-		type: [String], // come back later and replace this with a Mongoose object ID ,
+		type: [{type: mongoose.Schema.Types.ObjectId, ref: "User"}], 
 		required: false
 	},
 	headerImage: {
