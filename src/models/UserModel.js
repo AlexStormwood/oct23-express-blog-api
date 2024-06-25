4ac1fb4233476cdf7242c02fe2af8ce023a7aa16
Alex <alex.holder@bigfootds.com>
2024-06-25 19:57:40 +1000

    feat: user model has password and presave

--- src/models/UserModel.js
+++ src/models/UserModel.js
@@ -17,12 +17,42 @@ const userSchema = mongoose.Schema({
 		required: false, 
 		unique: false
 	},
+	password: {
+		type: String, 
+		required: true, 
+		unique: false
+	}
+	// isAdmin // boolean for role authorization 
 	// comments: {
 	// 	// These are NOT the same comments as what the Blogs contain, they just reuse the comment schema
 	// 	types: [commentSchema],
 	// 	required: false
 	// }
 });
+
+
+userSchema.pre(
+	"save",
+	async function (next) { 
+		const user = this;
+		console.log("Pre-save hook running.");
+
+		if (!user.isModified("password")){
+			return;
+		}
+
+		console.log("Pre-save hook runnin and password is modified!");
+		// if we reach this line of code, the password is modified
+		// and thus is not encrypted!
+		// we must encrypt it!
+
+		// TODO: encryption 
+
+
+		next();
+	}
+)
+
 
 
 const UserModel = mongoose.model("User", userSchema);
--- src/utils/seed.js
+++ src/utils/seed.js
@@ -6,13 +6,27 @@ const { databaseConnect, databaseClear, databaseClose } = require("./database");
 async function seedUsers () {
 	let userData = [
 		{
-			username: "alex"
+			username: "alex",
+			password: "password"
 		},
 		{
-			username: "pikachu"
+			username: "pikachu",
+			password: "pokemon"
 		}
 	];
 
+	let thirdUser = {
+		username: "callum",
+		password: "supercool"
+	}
+
+	console.log("Creating user with .create");
+	let callum = await UserModel.create(thirdUser);
+
+	console.log("Calling save on the created user:");
+	await callum.save();
+
+	console.log("Creating users from insertMany:");
 	let result = await UserModel.insertMany(userData)
 	console.log(result);
 	return result;
